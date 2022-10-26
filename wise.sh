195f8a0f40c791f0be920674cb1776afb1023350
Warren Gerald Polandra <85095564+warrenpolandra@users.noreply.github.com>
2022-10-26 15:24:29 +0700

    Update wise.sh

--- wise.sh
+++ wise.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
+
+# IP DNS
 echo "nameserver 192.168.122.1" > /etc/resolv.conf
+
+# install bind9
 apt-get update
 apt-get install bind9 -y
 
-echo 'zone "wise.d11.com" {
-type master;
-file "/etc/bind/wise/wise.d11.com";
-};'
-
 # nomor 2
+# zone wise.d11.com
 echo 'zone "wise.d11.com" {
         type master;
         file "/etc/bind/wise/wise.d11.com";
 };' 
 > /etc/bind/named.conf.local
+
 mkdir /etc/bind/wise
 
-echo "
-\$TTL    604800
+# nomor 2
+# konfigurasi alias  www.wise.d11.com
+echo "\$TTL    604800
 @       IN      SOA     wise.d11.com. root.wise.d11.com. (
-                                2       ; Serial
+                        202210261       ; Serial
                         604800          ; Refresh
                         86400           ; Retry
                         2419200         ; Expire
                         604800 )        ; Negative Cache TTL
 ;
 @               IN      NS      wise.d11.com.
-@               IN      A       192.190.1.2 ; IP Wise
-www             IN      CNAME   wise.d11.com.
-" 
-> /etc/bind/wise/wise.d11.com
+@               IN      A       192.190.2.2 ; IP WISE
+www             IN      CNAME   wise.d11.com." > /etc/bind/wise/wise.d11.com
+
 service bind9 restart
 
 # nomor 3
-echo "
-\$TTL    604800
+# konfigurasi www.eden.wise.d11.com
+echo "\$TTL    604800
 @       IN      SOA     wise.d11.com. root.wise.d11.com. (
-                                2       ; Serial
+                        202210261       ; Serial
                         604800          ; Refresh
                         86400           ; Retry
                         2419200         ; Expire
                         604800 )        ; Negative Cache TTL
 ;
 @               IN      NS      wise.d11.com.
-@               IN      A       192.190.1.2 ; IP Wise
+@               IN      A       192.190.2.2 ; IP WISE
 www             IN      CNAME   wise.d11.com.
-eden            IN      A       192.190.3.3 ; IP Eden
-www.eden        IN      CNAME   eden.wise.d11.com.
-" 
-> /etc/bind/wise/wise.d11.com
+eden            IN      A       192.190.3.3 ; IP EDEN
+www.eden        IN      CNAME   eden.wise.d11.com." > /etc/bind/wise/wise.d11.com
+
 service bind9 restart
 
 # nomor 4
-echo '
-zone "wise.d11.com" {
+# Reverse DNS
+echo 'zone "wise.d11.com" {
         type master;
         file "/etc/bind/wise/wise.d11.com";
 };
@@ -61,11 +61,9 @@ zone "wise.d11.com" {
 zone "2.190.192.in-addr.arpa" {
         type master;
         file "/etc/bind/wise/2.190.192.in-addr.arpa";
-};' 
-> /etc/bind/named.conf.local
+};' > /etc/bind/named.conf.local
 
-echo "
-\$TTL    604800
+echo "\$TTL    604800
 @       IN      SOA     wise.d11.com. root.wise.d11.com. (
                                 2       ; Serial
                         604800          ; Refresh
@@ -74,32 +72,30 @@ echo "
                         604800 )        ; Negative Cache TTL
 ;
 2.190.192.in-addr.arpa.   IN      NS      wise.d11.com.
-2                       IN      PTR     wise.d11.com.
-"
-> /etc/bind/wise/2.190.192.in-addr.arpa
+2                       IN      PTR     wise.d11.com." > /etc/bind/wise/2.190.192.in-addr.arpa
+
 service bind9 restart
 
-
 # nomor 5
-echo '
-zone "wise.d11.com" {
+# notify DNS Slave (Berlint)
+echo 'zone "wise.d11.com" {
         type master;
         notify yes;
-        also-notify {192.190.3.2;}; 
-        allow-transfer {192.190.3.2;};
+        also-notify {192.190.3.2;}; // IP BERLINT
+        allow-transfer {192.190.3.2;}; // IP BERLINT
         file "/etc/bind/wise/wise.d11.com";
 };
 
 zone "2.190.192.in-addr.arpa" {
         type master;
         file "/etc/bind/wise/2.190.192.in-addr.arpa";
-};' 
-> /etc/bind/named.conf.local
+};' > /etc/bind/named.conf.local
+
 service bind9 restart
 
 # nomor 6
-echo "
-\$TTL    604800
+# konfigurasi subdomain operation.wise.d11.com
+echo "\$TTL    604800
 @       IN      SOA     wise.d11.com. root.wise.d11.com. (
                                 2       ; Serial
                         604800          ; Refresh
@@ -108,117 +104,20 @@ echo "
                         604800 )        ; Negative Cache TTL
 ;
 @       IN      NS      wise.d11.com.
-@               IN      A       192.190.3.3 ; IP Eden
+@               IN      A       192.190.3.3 ; IP EDEN
 www             IN      CNAME   wise.d11.com.
-eden           IN      A       192.190.3.3 ; IP Eden
+eden           IN      A       192.190.3.3 ; IP EDEN
 www.eden       IN      CNAME   eden.wise.d11.com.
-ns1             IN      A       192.190.3.2; IP Berlint
-operation           IN      NS      ns1
-"> /etc/bind/wise/wise.d11.com
+ns1             IN      A       192.190.3.2; IP BERLINT
+operation           IN      NS      ns1" > /etc/bind/wise/wise.d11.com
 
-echo "
-options {
+# delegasi subdomain
+echo "options {
         directory \"/var/cache/bind\";
 
         allow-query{any;};
         auth-nxdomain no;    # conform to RFC1035
         listen-on-v6 { any; };
-};
-" 
-> /etc/bind/named.conf.options
+};" > /etc/bind/named.conf.options
 
-echo '
-zone "wise.d11.com" {
-        type master;
-        //notify yes;
-        //also-notify {192.190.3.2;}; 
-        file "/etc/bind/wise/wise.d11.com";
-        allow-transfer {192.190.3.2;}; 
-};
-
-zone "2.190.192.in-addr.arpa" {
-        type master;
-        file "/etc/bind/wise/2.190.192.in-addr.arpa";
-};
-' 
->  /etc/bind/named.conf.local
-
-service bind9 restart
-}
-
-Berlint(){
-echo "nameserver 192.168.122.1" > /etc/resolv.conf
-apt-get update
-
-# nomor 5
-apt-get update
-apt-get install bind9 -y
-echo '
-zone "wise.d11.com" {
-        type slave;
-        masters { 192.190.1.2; };
-        file "/var/lib/bind/wise.d11.com";
-};
-' 
-> /etc/bind/named.conf.local
-service bind9 restart
-
-# nomor 6
-echo "
-options {
-        directory \"/var/cache/bind\";
-        allow-query{any;};
-        auth-nxdomain no;    # conform to RFC1035
-        listen-on-v6 { any; };
-};
-" 
-> /etc/bind/named.conf.options
-echo '
-zone "wise.d11.com" {
-        type slave;
-        masters { 192.190.1.2; };
-        file "/var/lib/bind/wise.d11.com";
-};
-
-zone "operation.wise.d11.com"{
-        type master;
-        file "/etc/bind/operation/operation.wise.d11.com";
-};
-'
-> /etc/bind/named.conf.local
-mkdir /etc/bind/operation
-echo "
-\$TTL    604800
-@       IN      SOA     operation.wise.d11.com. root.operation.wise.d11.com. (
-                                2      ; Serial
-                        604800         ; Refresh
-                        86400         ; Retry
-                        2419200         ; Expire
-                        604800 )       ; Negative Cache TTL
-;
-@               IN      NS      operation.wise.d11.com.
-@               IN      A       192.190.3.3       ;ip Eden
-www             IN      CNAME   operation.wise.d11.com.
-" 
-> /etc/bind/operation/operation.wise.d11.com
-service bind9 restart
-
-
-# Nomor 7
-echo "
-\$TTL    604800
-@       IN      SOA     operation.wise.d11.com. root.operation.wise.d11.com. (
-                                2      ; Serial
-                        604800         ; Refresh
-                        86400         ; Retry
-                        2419200         ; Expire
-                        604800 )       ; Negative Cache TTL
-;
-@               IN      NS      operation.wise.d11.com.
-@               IN      A       192.190.3.3       ;ip Eden
-www             IN      CNAME   operation.wise.d11.com.
-strix         IN      A       192.190.3.3       ;IP Eden
-www.strix     IN      CNAME   strix.operation.wise.d11.com.
-" 
-> /etc/bind/operation/operation.wise.d11.com
 service bind9 restart
